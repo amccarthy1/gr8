06d72a8b22647f416251b3780f8b88d37065c144
Michael Washburn Jr <mdw7326@rit.edu>
2015-03-16 10:31:25 -0400

    Add UserForm

--- gr8/grading/forms.py
+++ gr8/grading/forms.py
@@ -7,3 +7,22 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user',)
+
+class UserForm(UserCreationForm):
+    """
+    Allows the creation of a new user, with fields:
+        - username
+        - first_name
+        - last_name
+        - email
+        - password1
+        - password2
+    """
+    #All fields are required
+    email = forms.EmailField(label='Email', required=True)
+    first_name = forms.CharField(label='First Name', required=True)
+    last_name = forms.CharField(label='Last Name', required=True)
+    
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
