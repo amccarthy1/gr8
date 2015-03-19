8541a18c93f036696883e4cfe468922acc97a9c6
Nicholas <njc3006@rit.edu>
2015-03-18 20:42:51 -0400

    Added from for the room model

--- gr8/grading/forms.py
+++ gr8/grading/forms.py
@@ -27,3 +27,8 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+
+class RoomForm(forms.ModelForm):
+
+    class Meta:
+        model = Room
